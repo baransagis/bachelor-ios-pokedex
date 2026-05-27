94448de3b75f6b391a54d31ab568cefad29d12c6
Baran Alexander Sagis <61687681+baransagis@users.noreply.github.com>
2026-05-27 23:38:47 +0200

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Baran Alexander Sagis on 27.05.26.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

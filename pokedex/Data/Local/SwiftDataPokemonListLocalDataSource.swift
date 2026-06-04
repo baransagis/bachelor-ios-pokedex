e7e23db0de16becaced7fa68578ebbac4def3ee4
Baran Alexander Sagis <61687681+baransagis@users.noreply.github.com>
2026-06-04 22:53:15 +0200

    removed unnecessary check for existing item in db

--- pokedex/Data/Local/SwiftDataPokemonListLocalDataSource.swift
+++ pokedex/Data/Local/SwiftDataPokemonListLocalDataSource.swift
@@ -9,25 +9,11 @@ actor SwiftDataPokemonListLocalDataSource: PokemonListLocalDataSource {
     }
 
     func insertPokemon(_ pokemon: [PokemonListItemDTO]) async throws {
-        var existingPokemonById = try fetchExistingPokemonById()
-
         for item in pokemon {
-            if let existingItem = existingPokemonById[item.id] {
-                existingItem.update(with: item)
-            } else {
                 let newItem = PokemonListItemEntity(dto: item)
                 modelContext.insert(newItem)
-                existingPokemonById[item.id] = newItem
-            }
         }
 
         try modelContext.save()
-    }
-
-    private func fetchExistingPokemonById() throws -> [Int: PokemonListItemEntity] {
-        let descriptor = FetchDescriptor<PokemonListItemEntity>()
-        let existingPokemon = try modelContext.fetch(descriptor)
-
-        return Dictionary(uniqueKeysWithValues: existingPokemon.map { ($0.id, $0) })
     }
 }
